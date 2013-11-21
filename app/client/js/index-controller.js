70fdcc68a6a7a21f5d7b3b141a8a0d35adf21b20
Daniel Belchamber <daniel.belchamber@gmail.com>
2013-11-21 11:53:01 -0500

    Can now Login to Facebook from the Index page.

--- app/client/js/index-controller.js
+++ app/client/js/index-controller.js
@@ -7,4 +7,9 @@
 
 $('.signup-facebook').on('click', function() {
 	console.log('I clicky the button');
+	FB.login(function(response) {
+		if (response.authResponse) {
+			window.location += 'home/';
+		}
+	});
 });
