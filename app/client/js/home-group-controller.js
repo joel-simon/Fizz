1e39b7be4ff7b6b2e4a198f276d384c45569410d
Daniel Belchamber <daniel.belchamber@gmail.com>
2014-01-16 17:36:50 -0500

    autocomplete almost done

--- app/client/js/home-group-controller.js
+++ app/client/js/home-group-controller.js
@@ -1,9 +1,12 @@
 var group = [];
 var friendNames = [];
+var groupNames =[];
+var nameToId = {};
 
 function loadGroup() {
 
 	var htmlString;
+	var friendCount = 0;
 
 	friends.forEach(function(friend, i) {
 		getFbData(friend.id, function(pic, name) {
@@ -11,8 +14,18 @@ function loadGroup() {
 				'<li id="group-'+friend.id+'" class="drag float-left">'+
 					'<img src="'+pic+'" title="'+name+'" class="pic">'+
 				'</li>';
-			$('#friends').append(htmlString);
-			friendNames.push(name);
+			if ( group.indexOf(friend.id) != -1 ) {
+				$('#group').append(htmlString);
+				groupNames.push(name);
+			}
+			else {
+				$('#friends').append(htmlString);
+				friendNames.push(name);
+			}
+
+			if ( nameToId[name] ) nameToId.push( friend.id );
+			else nameToId[name] = [ friend.id ];
+			
 			$('li.drag').draggable({
 				appendTo: "#groupManager",
 				revert: "invalid",
@@ -21,9 +34,37 @@ function loadGroup() {
 				cursor: "move",
 				scroll: "false"
 			});
+			if (++friendCount == friends.length) {
+				setAutoComplete();
+			}
 		});
 	});
 }
+
+function setAutoComplete() {
+	$( '#friendSearch' ).autocomplete({
+		source: friendNames
+	});
+	$( '#groupSearch' ).autocomplete({
+		source: groupNames
+	});
+}
+
+
+$('#friendSearch').on('input', function() {
+	// console.log(this.value);
+	var auto = $('.ui-autocomplete li a').map(function() { 
+		return $(this).text() 
+	}).get();
+	$('#friends li').addClass('hidden');
+	auto.forEach(function(name, i) {
+		var idArray = nameToId[name];
+		for (var i = 0; i < idArray.length; i++) {
+			$('group-'+idArray[i]).removeClass('hidden');
+		}
+	});
+});
+
 
 $('#group').droppable({
 	accept: "#friends li",
@@ -32,7 +73,12 @@ $('#group').droppable({
 		var friend = ui.draggable[0].id;
 		var fid = friend.replace('group-','');
 		group.push(fid);
-		// console.log(group);
+
+		getFbData(fid, function(pic, name) {
+			friendNames.splice( friendNames.indexOf(name), 1 );
+			groupNames.push(name);
+			setAutoComplete();
+		});
 	}
 });
 
@@ -46,6 +92,13 @@ $('#friends').droppable({
 		if (index > -1) {
 			group.splice(index, 1);
 		}
-		// console.log(group);
+
+		getFbData(fid, function(pic, name) {
+			groupNames.splice( groupNames.indexOf(name), 1 );
+			friendNames.push(name);
+			setAutoComplete();
+		});
 	}
 });
+
+
