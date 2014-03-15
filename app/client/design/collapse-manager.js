4fcc880a89e3ba5b5f0e9cfdaf225f99ffb545f7
Daniel Belchamber <daniel.belchamber@gmail.com>
2014-03-15 13:30:14 -0400

    Incorrect collapse view

--- app/client/design/collapse-manager.js
+++ app/client/design/collapse-manager.js
@@ -5,19 +5,27 @@
 */
 ////////////////////////////////////////////////////////////////////////////////
 
+var collapse = false;
+
 $('#collapse').on('click', function() {
-	$('#collapse').addClass('hidden');
-	$('#collapse-list').removeClass('hidden');
-	$('#thread-list').addClass('hidden');
+	if ( $(window).width() < 500 ) {
+		$('#collapse').addClass('hidden');
+		$('#collapse-list').removeClass('hidden');
+		$('#thread-list').addClass('hidden');
+		collapse = true;
+	}
 });
 
 $('#collapse-list li').on('click', function() {
 	var lid = $(this)[0].id;
 	var thread = lid.substr(lid.length-1);
 	
-	$('#collapse').removeClass('hidden');
-	$('#collapse-list').addClass('hidden');
-	$('#thread-list').removeClass('hidden');
+	if ( $(window).width() < 500 ) {
+		$('#collapse').removeClass('hidden');
+		$('#collapse-list').addClass('hidden');
+		$('#thread-list').removeClass('hidden');
+		collapse = false;
+	}
 
-	SM.scrollToThread(thread);
+	if (!detail) SM.scrollToThread(thread);
 });
