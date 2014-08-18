8a3d42a589d2cf901fe3042d93b09eb79efd0f53
Sloth6 <joelsimon6@gmail.com>
2014-08-18 12:18:59 -0400

    refactored fizzTypes

--- app/server/fizzTypes.js
+++ app/server/fizzTypes.js
@@ -1,22 +1,19 @@
-var latlng = {
-  lat: 'number',
-  lng: 'number'
-}
+
 var pn = function(n) {
   var regexp = /^[\s()+-]*([0-9][\s()+-]*){6,20}$/
   return regexp.test(n);
 }
 
-
 module.exports = {
-  user: function(u) {
-    if (!u) throw (' No user object passed');
-    if ((typeof u.uid !== 'number') || u.uid === 0) throw('Bad uid:'+uid)
-    if ((typeof u.pn !== 'string')) throw('Bad Pn:'+pn);
-    if ((typeof u.name !== 'string')) throw('Bad name:'+name);
-    return true;
+  user: {
+    uid : 'posInt',
+    pn  : pn,
+    name: 'string'
   },
-  latlng: latlng,
+  latlng: {
+    lat: 'number',
+    lng: 'number'
+  },
   message: {
     mid: 'posInt',
     eid: 'posInt',
