046bcd2823954ccc5b0df6250ba9b24ce9ebe35d
Antonio Ono <me@antonioono.com>
2014-12-17 20:04:54 -0500

    clicking button toggles RSVP; modal can be closed

--- app/client/scripts/client.js
+++ app/client/scripts/client.js
@@ -1,37 +1,44 @@
 var userHtml;
 $(document).ready(function (argument) {
-	userHtml = $('.guestlist >>').get().filter(function(li){
-		return $(li).text() == user.name;
-	})[0]
-});
+    userHtml = $('.guestlist >>').get().filter(function(li){
+        return $(li).text() == user.name;
+    })[0]
 
-$(document).on('touchstart click', function(e) {
-	if (accepted && confirm('Leave this event?')) {
-		console.log('test');
-		$.ajax({
-		  type: "POST",
-		  url: '/leave',
-		  data: { key: key },
-		  success: function (foo) {
-		  	console.log($('.rsvp h1'));
-		  	$('.noReply').append(userHtml)
-				$('.rsvp h1').text('Click anywhere to join')
-		  	accepted = false;
-		  },
-		  fail: function(err){
-		  	console.log('there was an error', err);
-		  }
-		});
-	} else if (confirm('Join this event?')) {
-		$.ajax({
-		  type: "POST",
-		  url: '/Join',
-		  data: { key: key },
-		  success: function (foo) {
-		  	$('.rsvp h1').text('You are attending')
-		  	$('.interested').append(userHtml)
-		  	accepted = true;
-		  }
-		});
-	}
+    $(".rsvp a").on('touchstart click', function(e) {
+        if (accepted && confirm('Leave this event?')) {
+            console.log('test');
+            $.ajax({
+                type: "POST",
+                url: '/leave',
+                data: { key: key },
+                success: function (foo) {
+                    console.log($('.rsvp h1'));
+                    $('.noReply').append(userHtml)
+                    $('.rsvp h1').text('Click anywhere to join')
+                    accepted = false;
+                },
+                fail: function(err){
+                    console.log('there was an error', err);
+                }
+            });
+        } else if (confirm('Join this event?')) {
+            $.ajax({
+                type: "POST",
+                url: '/Join',
+                data: { key: key },
+                success: function (foo) {
+                    $('.rsvp h1').text('You are attending')
+                    $('.interested').append(userHtml)
+                    accepted = true;
+                }
+            });
+            
+            // Open modal
+            $(".modal").addClass("open");
+        }
+    });
+    
+    $(".modal").on("touchstart click", function(e) {
+        $(this).removeClass("open");
+    });
 });
